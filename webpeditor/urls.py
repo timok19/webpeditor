f369096e6a47b34f6587dfee07447b3a5444929e
Temirkhan Amanzhanov <tima1199@gmail.com>
2023-01-30 16:40:55 +0100

    Visual layout setup + linking pages

--- webpeditor/urls.py
+++ webpeditor/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', include('webpeditor_app.urls'))
 ]
