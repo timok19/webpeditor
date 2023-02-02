d7a3292e14fec768db7f3b3503653d035635fde3
Temirkhan Amanzhanov <tima1199@gmail.com>
2023-02-02 15:27:53 +0100

    Added edited_image_api() function to store edited image objects into db + added documentation for methods

--- webpeditor_app/views/views.py
+++ webpeditor_app/views/views.py
@@ -6,6 +6,9 @@ from django.http.response import JsonResponse
 from webpeditor_app.models.database.models import OriginalImage
 from webpeditor_app.models.database.serializers import OriginalImageSerializer
 
+from webpeditor_app.models.database.models import EditedImage
+from webpeditor_app.models.database.serializers import EditedImageSerializer
+
 
 def index(request):
     return render(request, 'index.html')
@@ -13,6 +16,21 @@ def index(request):
 
 @csrf_exempt
 def original_image_api(request, _id=0):
+    """Function implementation of API methods to store original_image into DB
+
+    Parameters:
+        request: str
+
+            API request (GET, POST, PUT, DELETE)
+
+        _id: int
+
+            image_id to delete using DELETE API method
+
+    Returns:
+        json_response: JsonResponse()
+    """
+
     if request.method == 'GET':
         original_images = OriginalImage.objects.all()
         original_image_serializer = OriginalImageSerializer(original_images, many=True)
@@ -48,3 +66,56 @@ def original_image_api(request, _id=0):
 
         return JsonResponse("Deleted successfully", safe=False)
 
+
+@csrf_exempt
+def edited_image_api(request, _id=0):
+    """Function implementation of API methods to store edited_image into DB
+
+    Parameters:
+        request: str
+
+            API request (GET, POST, PUT, DELETE)
+
+        _id: int
+
+            image_id to delete using DELETE API method
+
+    Returns:
+        json_response: JsonResponse()
+    """
+    if request.method == 'GET':
+        edited_images = EditedImage.objects.all()
+        edited_image_serializer = EditedImageSerializer(edited_images, many=True)
+
+        return JsonResponse(edited_image_serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        edited_image_data = JSONParser().parse(request)
+        edited_image_serializer = EditedImageSerializer(data=edited_image_data)
+
+        if edited_image_serializer.is_valid():
+            edited_image_serializer.save()
+
+            return JsonResponse("Added edited image to db successfully", safe=False)
+
+        return JsonResponse("Failed to add edited image into db", safe=False)
+
+    elif request.method == 'PUT':
+        edited_image_data = JSONParser().parse(request)
+        edited_image = EditedImage.objects.get(image_id=edited_image_data['image_id'])
+        edited_image_serializer = EditedImageSerializer(edited_image, data=edited_image_data)
+
+        if edited_image_serializer.is_valid():
+            edited_image_serializer.save()
+
+            return JsonResponse("Updated edited image in db successfully", safe=False)
+
+        return JsonResponse("Failed to update edited image in db", safe=False)
+
+    elif request.method == 'DELETE':
+        edited_image = EditedImage.objects.get(image_id=_id)
+        edited_image.delete()
+
+        return JsonResponse("Deleted edited image successfully", safe=False)
+
+# TODO: test the edited_image_api()
