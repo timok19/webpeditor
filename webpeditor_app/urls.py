b23c940b52d9dc67b47b69affa2c6ee26f047a93
Temirkhan Amanzhanov <tima1199@gmail.com>
2023-02-02 14:09:32 +0100

    Added path for deleting image object in db

--- webpeditor_app/urls.py
+++ webpeditor_app/urls.py
@@ -5,5 +5,6 @@ from webpeditor_app.views.views import index
 
 urlpatterns = [
     re_path(r'^$', index, name='index'),
-    re_path(r'^original_image/?$', original_image_api)
+    re_path(r'^original_image/?$', original_image_api),
+    re_path(r'^original_image/([0-9]+)$', original_image_api)
 ]
