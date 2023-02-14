f82bb62aa722cdbf1bc0beff01cb845bdb95fd0b
Temirkhan Amanzhanov <tima1199@gmail.com>
2023-02-14 10:37:09 +0100

    Added testing version of update_session()

--- webpeditor_app/services/image_services/session_id_to_db.py
+++ webpeditor_app/services/image_services/session_id_to_db.py
@@ -1,3 +1,6 @@
+import shutil
+from pathlib import Path
+
 from django.contrib.sessions.models import Session
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import JsonResponse
@@ -6,9 +9,6 @@ from django.utils import timezone
 from webpeditor import settings
 from webpeditor_app.models.database.models import OriginalImage
 
-from pathlib import Path
-import shutil
-
 
 def set_session_expiry(request: WSGIRequest):
     # Set session_id token expiry to 2 hours
@@ -16,31 +16,69 @@ def set_session_expiry(request: WSGIRequest):
 
 
 def update_session(session_id: str) -> JsonResponse:
-    session = Session.objects.filter(session_key=session_id).first()
-    original_image = OriginalImage.objects.filter(session_id=session_id).first()
+    session: Session = Session.objects.filter(session_key=session_id).first()
+    original_image: OriginalImage = OriginalImage.objects.filter(session_id=session_id).first()
+    path_to_old_image_folder: Path = Path(settings.MEDIA_ROOT) / session_id
 
-    if timezone.now() > session.expire_date:
-
-        if original_image:
+    if original_image:
+        if timezone.now() > original_image.created_at:
             original_image.delete()
-            path_to_old_image_folder = Path(settings.MEDIA_ROOT) / session_id
-
             if path_to_old_image_folder.exists():
                 shutil.rmtree(path_to_old_image_folder)
-
-        if session:
-            session.delete()
-
-        return JsonResponse({'success': True,
-                             'info': 'Session has been expired and image has been deleted'},
-                            status=204)
-
+            return JsonResponse({'success': True,
+                                 'info': 'Session has been expired and image has been deleted'},
+                                status=204)
+        else:
+            session.expire_date = timezone.now() + timezone.timedelta(seconds=7200)
+            session.save()
+            print(f"\nSession will expire at {session.expire_date}")
+            return JsonResponse({'success': True,
+                                 'info': 'Session is alive',
+                                 'estimated_time_of_session_id': str(session.expire_date)},
+                                status=200)
     else:
-        session.expire_date = timezone.now() + timezone.timedelta(seconds=7200)
-        session.save()
-        print(f"\nSession will expire at {session.expire_date}")
+        if path_to_old_image_folder.exists():
+            shutil.rmtree(path_to_old_image_folder)
 
-        return JsonResponse({'success': True,
-                             'info': 'Session is alive',
-                             'estimated_time_of_session_id': str(session.expire_date)},
-                            status=200)
+        if not session:
+            return JsonResponse({'success': False,
+                                 'error': 'Invalid session_id'},
+                                status=400)
+        else:
+            if timezone.now() < session.expire_date:
+                session.expire_date = timezone.now() + timezone.timedelta(seconds=7200)
+                session.save()
+                print(f"\nSession will expire at {session.expire_date}")
+                return JsonResponse({'success': True,
+                                     'info': 'Session is alive',
+                                     'estimated_time_of_session_id': str(session.expire_date)},
+                                    status=200)
+            else:
+                session.delete()
+                return JsonResponse({'success': True,
+                                     'info': 'Session has been expired and image folder has been deleted'},
+                                    status=204)
+
+# def update_session(session_id: str) -> JsonResponse:
+#     session = Session.objects.filter(session_key=session_id).first()
+#     original_image = OriginalImage.objects.filter(session_id=session_id).first()
+#     path_to_old_image_folder = Path(settings.MEDIA_ROOT) / session_id
+#
+#     if not session:
+#         return JsonResponse({'success': False, 'error': 'Invalid session_id'}, status=400)
+#
+#     if path_to_old_image_folder.exists():
+#         shutil.rmtree(path_to_old_image_folder)
+#
+#     if original_image and timezone.now() > original_image.created_at:
+#         original_image.delete()
+#         return JsonResponse({'success': True, 'info': 'Session has been expired and image has been deleted'},
+#                             status=204)
+#
+#     session.expire_date = timezone.now() + timezone.timedelta(
+#         seconds=7200) if timezone.now() < session.expire_date else session.delete() or JsonResponse(
+#         {'success': True, 'info': 'Session has been expired and image folder has been deleted'}, status=204)
+#     print(f"\nSession will expire at {session.expire_date}")
+#     return JsonResponse(
+#         {'success': True, 'info': 'Session is alive', 'estimated_time_of_session_id': str(session.expire_date)},
+#         status=200)
