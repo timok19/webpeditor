ba01c989c5bb3f90e6fb46bbeec6e9f4da44205b
Temirkhan Amanzhanov <tima1199@gmail.com>
2023-04-22 02:17:04 +0200

    Update settings.py

--- webpeditor/settings.py
+++ webpeditor/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '*']
 
 # Application definition
 # In development mode. Delete this in production mode (add domains in white list)
