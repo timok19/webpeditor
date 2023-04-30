f8d1b8b3fffc7eb39a5218036e6837ec9be63921
Temirkhan Amanzhanov <tima1199@gmail.com>
2023-04-30 05:36:16 +0200

    Changed from modal to tooltip

--- static/js/services/openImageModal.js
+++ static/js/services/openImageModal.js
@@ -1,6 +1,5 @@
 const modal = document.getElementById("modal");
 const modalImg = document.getElementById("modal-img");
-const infoImageModal = document.getElementById("defaultModal");
 
 if (document.querySelector("[modal-backdrop]") !== null) {
   document.querySelector("[modal-backdrop]").setAttribute("class", "hidden bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40");
@@ -15,25 +14,8 @@ function closeImageModal() {
   modal.classList.add("hidden");
 }
 
-function showInfoImageModal() {
-  infoImageModal.classList.remove("hidden");
-  if (document.querySelector("[modal-backdrop]") !== null) {
-    document.querySelector("[modal-backdrop]")
-      .setAttribute("class", "bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40");
-  }
-}
-
-function closeInfoImageModal() {
-  infoImageModal.classList.add("hidden");
-  if (document.querySelector("[modal-backdrop]") !== null) {
-    document.querySelector("[modal-backdrop]")
-      .setAttribute("class", "hidden bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40");
-  }
-}
-
 window.addEventListener("keydown", (event) => {
   if (event.key === "Escape" || event.key === "Esc") {
     closeImageModal();
-    closeInfoImageModal();
   }
 });
