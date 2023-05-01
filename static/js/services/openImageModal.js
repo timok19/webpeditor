418c5f420d9e87ce93ea3061f2c69c4869d64460
Temirkhan Amanzhanov <tima1199@gmail.com>
2023-05-01 13:42:51 +0200

    Little fixes on FE

--- static/js/services/openImageModal.js
+++ static/js/services/openImageModal.js
@@ -1,13 +1,17 @@
-const modal = document.getElementById("modal");
-const modalImg = document.getElementById("modal-img");
+const modal = document.getElementById("modal-image");
+
+const imageIdElement = document.getElementById("imageId");
+let imageId = imageIdElement ? JSON.parse(imageIdElement.textContent) : null;
+const image = document.getElementById(imageId)
 
 if (document.querySelector("[modal-backdrop]") !== null) {
   document.querySelector("[modal-backdrop]").setAttribute("class", "hidden bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40");
 }
 
 function showImageModal(src) {
+  console.log(src)
   modal.classList.remove("hidden");
-  modalImg.src = src;
+  image.src = src;
 }
 
 function closeImageModal() {
