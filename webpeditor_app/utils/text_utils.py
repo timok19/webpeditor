1880d47f909017a7ec348cbcb0685f07ca639711
Temirkhan Amanzhanov <tima1199@gmail.com>
2023-05-09 23:47:38 +0200

    Update text_utils.py

--- webpeditor_app/utils/text_utils.py
+++ webpeditor_app/utils/text_utils.py
@@ -2,10 +2,7 @@ import re
 
 
 def replace_with_underscore(file_name: str) -> str:
-    # Define the pattern for spaces and special characters
     pattern = re.compile(r'[\s!@#%$&^*/{}\[\]+<>,?;:`~]+')
+    new_file_name = re.sub(pattern, '_', file_name)
 
-    # Replace spaces and special characters with an underscore character
-    file_name = re.sub(pattern, '_', file_name)
-
-    return file_name
+    return new_file_name
