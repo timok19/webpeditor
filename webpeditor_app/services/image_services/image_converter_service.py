8babc5117f675f4425e2fd9d4a43919ac0a3a501
Amanzhanov Temirkhan <tima1199@gmail.com>
2023-05-16 13:30:11 +0200

    Update image_converter_service.py

--- webpeditor_app/services/image_services/image_converter_service.py
+++ webpeditor_app/services/image_services/image_converter_service.py
@@ -27,7 +27,7 @@ from webpeditor_app.utils.text_utils import replace_with_underscore
 def convert_image(image_file: InMemoryUploadedFile, quality: int, output_format: str) \
         -> Tuple[ImageClass, ImageClass, BytesIO]:
     buffer = BytesIO()
-    min_quality, max_quality, max_safe_quality = 1, 100, 95
+    min_quality, max_quality, max_safe_quality = 5, 100, 95
 
     pil_image: ImageClass = PilImage.open(image_file)
 
