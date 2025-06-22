0cd64b78c6ce72981648862cdc6fb819661f8cb5
Temirkhan Amanzhanov <tima1199@gmail.com>
2025-06-22 05:13:51 +0200

    Update error_handling.py

--- webpeditor_app/middlewares/error_handling.py
+++ webpeditor_app/middlewares/error_handling.py
@@ -1,6 +1,7 @@
 from anydi.ext.django import container
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse, HttpResponseBase
+from expression import Option
 from ninja.responses import codes_4xx, codes_5xx
 from pydantic import ValidationError
 from types_linq import Enumerable
@@ -8,8 +9,6 @@ from typing import Any, Callable, final, Final
 
 from webpeditor_app.controllers.schemas import HTTPResult
 from webpeditor_app.core.abc.webpeditor_logger_abc import WebPEditorLoggerABC
-from webpeditor_app.core.result.context_result import ContextResult
-from webpeditor_app.core.result.error_context import ErrorContext
 
 
 @final
@@ -21,7 +20,7 @@ class ErrorHandlingMiddleware:
     def __call__(self, request: HttpRequest) -> HttpResponseBase:
         response = self.get_response(request)
         return (
-            self.__process_error_response(request, response)
+            self.__process(request, response)
             if isinstance(response, HttpResponse) and self.__is_error_response(response.status_code)
             else response
         )
@@ -30,25 +29,25 @@ class ErrorHandlingMiddleware:
     def __is_error_response(status_code: int) -> bool:
         return Enumerable(codes_4xx).union(codes_5xx).any(lambda code: status_code == code)
 
-    def __process_error_response(self, request: HttpRequest, response: HttpResponse) -> HttpResponse:
+    def __process(self, request: HttpRequest, response: HttpResponse) -> HttpResponse:
         validation_result = self.__validate_json(request, response)
 
         # Skip error logging if a response object is not "HTTPResult"
-        if validation_result.is_error():
+        if validation_result.is_none():
             return response
 
-        http_errors = Enumerable(validation_result.ok.errors)
+        http_errors = Enumerable(validation_result.some.errors)
         message = f"Errors: [{'; '.join(http_errors.select(lambda error: f'(Message: "{error.message}" | Reasons: [{", ".join(error.reasons)}])'))}]"
 
         self.__logger.log_request_error(request, message)
 
         return response
 
-    def __validate_json(self, request: HttpRequest, response: HttpResponse) -> ContextResult[HTTPResult[Any]]:
+    def __validate_json(self, request: HttpRequest, response: HttpResponse) -> Option[HTTPResult[Any]]:
         try:
-            return ContextResult[HTTPResult[Any]].success(HTTPResult[Any].model_validate_json(response.content))
+            return Option[HTTPResult[Any]].Some(HTTPResult[Any].model_validate_json(response.content))
         except ValidationError:
-            return ContextResult[HTTPResult[Any]].failure(ErrorContext.server_error())
+            return Option[HTTPResult[Any]].Nothing()
         except Exception as exception:
             self.__logger.log_request_exception(request, exception, f"Unhandled error. Reason: '{response.text}'")
-            return ContextResult[HTTPResult[Any]].failure(ErrorContext.server_error())
+            return Option[HTTPResult[Any]].Nothing()
