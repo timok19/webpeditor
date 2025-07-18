b5a324947a7fbc621889100665f2765feabebdb5
Temirkhan Amanzhanov <tima1199@gmail.com>
2025-07-18 19:15:21 +0500

    Update Django model type annotations and dependencies
    Updated type annotations in Django models to use single type parameters as per latest stubs. Bumped 'django-types' to 0.22.0 and added 'django-unfold' to dependencies in pyproject.toml.

--- webpeditor_app/infrastructure/database/models/api.py
+++ webpeditor_app/infrastructure/database/models/api.py
@@ -5,6 +5,6 @@ from django.utils import timezone
 
 # TODO: make sure that APIKey is being added into database via Django Admin
 class APIKey(models.Model):
-    email: models.EmailField[str, str] = models.EmailField(max_length=50, unique=True)
-    key_hash: models.CharField[str, str] = models.CharField(max_length=128, unique=True)
-    created_at: models.DateTimeField[datetime, datetime] = models.DateTimeField(default=timezone.now)
+    email: models.EmailField[str] = models.EmailField(max_length=50, unique=True)
+    key_hash: models.CharField[str] = models.CharField(max_length=128, unique=True)
+    created_at: models.DateTimeField[datetime] = models.DateTimeField(default=timezone.now)
--- webpeditor_app/infrastructure/database/models/app_user.py
+++ webpeditor_app/infrastructure/database/models/app_user.py
@@ -8,9 +8,9 @@ from webpeditor_app.infrastructure.database.utils import generate_id
 
 
 class AppUser(models.Model):
-    id: models.CharField[str, str] = models.CharField(primary_key=True, default=generate_id, editable=False, max_length=128)
-    session_key: models.CharField[Optional[str], str] = models.CharField(max_length=128, null=True)
-    session_key_expiration_date: models.DateTimeField[datetime, datetime] = models.DateTimeField(default=timezone.now)
+    id: models.CharField[str] = models.CharField(primary_key=True, default=generate_id, editable=False, max_length=128)
+    session_key: models.CharField[Optional[str]] = models.CharField(max_length=128, null=True)
+    session_key_expiration_date: models.DateTimeField[datetime] = models.DateTimeField(default=timezone.now)
 
     def __str__(self) -> str:
         return str(self.id)
