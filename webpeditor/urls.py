1dfcff27c96a572c45258e73d0554750172d2425
Temirkhan Amanzhanov <tima1199@gmail.com>
2025-08-05 23:39:28 +0200

    Update urls.py

--- webpeditor/urls.py
+++ webpeditor/urls.py
@@ -13,8 +13,8 @@ from django.views.generic import TemplateView
 
 from webpeditor_app.views.content_not_found_view import ContentNotFoundView
 
-# Admin
 urlpatterns: list[Union[URLResolver, URLPattern]] = [
+    # Admin
     path("admin/doc/", include("django.contrib.admindocs.urls")),
     # TODO: Make dynamic URL Admin page access based on OTP code -> check out the perplexity chat
     path("admin/", admin.site.urls),
@@ -40,7 +40,11 @@ urlpatterns: list[Union[URLResolver, URLPattern]] = [
         name="password_reset_complete",
     ),
     path("accounts/login/", LoginView.as_view(extra_context={"site_header": admin.site.site_header}), name="login"),
-    path("accounts/profile/", TemplateView.as_view(template_name="registration/profile.html", extra_context={"site_header": admin.site.site_header}), name="profile"),
+    path(
+        "accounts/profile/",
+        TemplateView.as_view(template_name="registration/profile.html", extra_context={"site_header": admin.site.site_header}),
+        name="profile",
+    ),
     # WebP Editor
     path("", include("webpeditor_app.urls")),
     # For all non-existing (not allowed) urls
