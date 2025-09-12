fe287171329d87db3f1539ccbc3332975953181b
Temirkhan Amanzhanov <tima1199@gmail.com>
2025-09-12 15:56:49 +0200

    Refactor session service to use async session methods
    Replaces custom async wrappers with direct calls to Django's async session methods in SessionService. Updates dependencies in requirements files to latest versions for compatibility.

--- webpeditor_app/common/session/session_service.py
+++ webpeditor_app/common/session/session_service.py
@@ -1,7 +1,5 @@
-from datetime import timedelta, datetime
-from typing import Any, ClassVar, Final, Union, final, Optional
+from typing import ClassVar, Final, final
 
-from asgiref.sync import sync_to_async
 from django.contrib.sessions.backends.db import SessionStore
 from django.http.request import HttpRequest
 from django.utils import timezone
@@ -38,7 +36,8 @@ class SessionService:
     async def __acreate_user_id(self) -> ContextResult[str]:
         user_id = self.__generate_id()
         signed_user_id = self.__user_service.sign_id(user_id)
-        await self.__aset_item(self.__user_id_key, signed_user_id)
+        await self.__request.session.aset(self.__user_id_key, signed_user_id)
+        await self.__request.session.asave()
         return await self.__aget_user_id()
 
     @as_awaitable_result
@@ -46,7 +45,7 @@ class SessionService:
         return await (
             ContextResult[str]
             .from_result(
-                Option.of_optional(await self.__aget_item(self.__user_id_key))
+                Option.of_optional(await self.__request.session.aget(self.__user_id_key))
                 .to_result(ErrorContext.not_found(f"Unable to find User in the {SessionStore.__name__}!"))
                 .bind(self.__user_service.unsign_id)
             )
@@ -54,27 +53,13 @@ class SessionService:
         )
 
     async def __arefresh_expiry(self) -> None:
-        expire_at = timezone.now() + timedelta(seconds=self.__session_lifetime_in_seconds)
-        await self.__aset_item(self.__expire_at_key, expire_at.timestamp())
-        await self.__aset_expiry(self.__session_lifetime_in_seconds)
-        return None
-
-    async def __aset_expiry(self, value: Optional[Union[datetime, int]]) -> None:
-        await sync_to_async(self.__request.session.set_expiry)(value)
-        await sync_to_async(self.__request.session.save)()
+        await self.__request.session.aset_expiry(self.__session_lifetime_in_seconds)
+        await self.__request.session.asave()
         return None
 
     async def __ais_expired(self) -> bool:
-        expire_at = await self.__aget_item(self.__expire_at_key)
-        return not expire_at or timezone.now().timestamp() > expire_at
-
-    async def __aget_item(self, key: str) -> Optional[Any]:
-        return await sync_to_async(self.__request.session.get)(key)
-
-    async def __aset_item(self, key: str, value: Any) -> None:
-        await sync_to_async(self.__request.session.__setitem__)(key, value)
-        await sync_to_async(self.__request.session.save)()
-        return None
+        expire_at = await self.__request.session.aget_expiry_date()
+        return timezone.now() > expire_at
 
     @staticmethod
     def __generate_id() -> str:
