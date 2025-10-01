eb2a8a255fbea70adb4729f35db85ce8816056cc
Temirkhan Amanzhanov <tima1199@gmail.com>
2025-10-01 17:35:22 +0200

    Rename _get_root_path to _get_root_folder
    Refactored method name from _get_root_path to _get_root_folder in FilesRepositoryABC and ConverterFilesRepository for clarity and consistency.

--- webpeditor_app/infrastructure/repositories/converter_files_repository.py
+++ webpeditor_app/infrastructure/repositories/converter_files_repository.py
@@ -16,19 +16,19 @@ class ConverterFilesRepository(FilesRepositoryABC):
 
     @as_awaitable_result
     async def aupload_file(self, user_id: str, relative_path: str, content: bytes) -> ContextResult[HttpUrl]:
-        public_id = f"{self._get_root_path(user_id)}/{relative_path}"
+        public_id = f"{self._get_root_folder(user_id)}/{relative_path}"
         return await self.__cloudinary_client.aupload_file(public_id, content).map(lambda response: response.secure_url)
 
     @as_awaitable_result
     async def aget_zip_folder(self, user_id: str) -> ContextResult[HttpUrl]:
-        root_folder_path = self._get_root_path(user_id)
+        root_folder_path = self._get_root_folder(user_id)
         folder_path = f"{root_folder_path}/converter"
         zip_path = f"{root_folder_path}/webpeditor_converted.zip"
         return await self.__cloudinary_client.agenerate_zip_archive(folder_path, zip_path).map(lambda response: response.secure_url)
 
     @as_awaitable_result
     async def acleanup(self, user_id: str) -> ContextResult[None]:
-        root_folder_path = self._get_root_path(user_id)
+        root_folder_path = self._get_root_folder(user_id)
         return await (
             self.__cloudinary_client.adelete_folder_recursively(root_folder_path)
             .map(lambda response: self.__logger.info(f"Deleted {len(response.deleted.values())} files from '{root_folder_path}'", depth=5))
@@ -36,7 +36,7 @@ class ConverterFilesRepository(FilesRepositoryABC):
         )
 
     @staticmethod
-    def _get_root_path(user_id: str) -> str:
+    def _get_root_folder(user_id: str) -> str:
         return f"{user_id}/converter"
 
     # async def adelete_all_folders(self) -> None:
