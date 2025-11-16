dbb76df2014012f6f6289236fd3eaa73bf99f2b1
Temirkhan Amanzhanov <tima1199@gmail.com>
2025-11-16 14:42:42 +0100

    Rename

--- infrastructure/utils.py
+++ infrastructure/utils.py
@@ -4,9 +4,9 @@ import secrets
 
 class APIKeyUtils:
     @staticmethod
-    def create_api_key() -> str:
+    def create() -> str:
         return secrets.token_urlsafe(32)
 
     @staticmethod
-    def hash_api_key(api_key: str) -> str:
+    def hash(api_key: str) -> str:
         return hashlib.sha256(api_key.encode()).hexdigest()
